659d50a73aa1040bbd573911f69fad615ca92cce
Wan Ching <wanching_97@hotmail.com>
2017-11-30 23:25:11 +0800

    git init

---
+++ app/src/main/java/com/wanching/gmap/MainActivity.java
@@ -0,0 +1,34 @@
+package com.wanching.gmap;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String searchWord;
+    private Button btnSearch;
+    private EditText etSearch;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSearch = (Button) findViewById(R.id.btnSearch);
+        etSearch = (EditText) findViewById(R.id.search);
+        searchWord = etSearch.getText().toString();
+
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+                intent.putExtra("search", searchWord);
+                startActivity(intent);
+            }
+        });
+    }
+}
